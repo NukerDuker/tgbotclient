c200206fe1314edc65859e98df2ae4525c13ac26
idi_ot_suda <89637523326@list.ru>
2025-05-31 19:06:25 +0300

    initial commit

---
+++ src/main/kotlin/ru/sudokakado/tgbotclient/models/Update.kt
@@ -0,0 +1,4 @@
+package ru.sudokakado.tgbotclient.models
+
+class Update {
+}
---
+++ src/test/kotlin/ru/sudokakado/tgbotclient/TgbotclientApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.sudokakado.tgbotclient
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TgbotclientApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
